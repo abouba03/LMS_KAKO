8e9b5ad688d7571829afa24b442f323e5d7b6ed0
abouba03 <abouba1703@yandexx.ru>
2024-02-15 02:44:28 +0300

    I made a Dynamic Layout

---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,9 @@
+const CoursesPage = () => {
+    return (    
+        <div className="">
+            Cours page
+        </div>
+     );
+}
+ 
+export default CoursesPage;
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,35 @@
-"use-client"
+"use-client" 
 
 import { UserButton } from "@clerk/nextjs"
+import { usePathname } from "next/navigation"
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 export const NavbarRoutes = () => {
+    const pathname =usePathname();
+
+    const isTeacherPage = pathname?.startsWith("/teacher");
+    const isPlayerPage = pathname?.includes("/chapter");
     return (
         <div className="flex gap-x-2 ml-auto">
-            <UserButton />
+            {isTeacherPage || isPlayerPage ? (
+                <Link href="/">
+                    <Button className="sm" variant="ghost">
+                        <LogOut className="h-4 w-4 mr-2"/>
+                            Quitter
+                    </Button>
+                </Link>
+            ) : (
+                <Link href="/teacher/courses">
+                    <Button className="sm" variant="ghost">
+                        Mode Professeur
+                    </Button>
+                </Link>
+            )}
+            <UserButton 
+                afterSignOutUrl="/"
+            />
         </div>
     )
 }
