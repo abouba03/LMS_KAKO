9b66c495b7fde6461eb12ad63bd63b3845db3ba1
abouba17 <abouba03@yandex.ru>
2024-02-24 23:17:56 +0300

    dd

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import Sidebar from "./_components/sidebar";
 
 const DashboardLayout = ({children}: {children:React.ReactNode;}) => {
     return ( 
-        <div className="h-full j">
+        <div className="h-full iu">
             <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full">
                 <Navbar />
             </div>
