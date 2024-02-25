23aa7714807c4f0991f679a15890683eaf4171e5
abouba03 <abouba1703@yandex.ru>
2024-02-26 00:40:23 +0300

    Creation de la page de new courses

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
     return (    
-        <div className="">
-            Cours page
+        <div className="p-6">
+            <Link href="/teacher/create">
+                <Button>
+                    New Course
+                </Button>
+            </Link>
+           
         </div>
      );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider } from "@clerk/nextjs";
+import { ToastProvider } from "@/components/providers/toaster-provider";
 
 
 
@@ -20,7 +21,10 @@ export default function RootLayout({
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="fr">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToastProvider/>
+          {children}
+          </body>
       </html>
     </ClerkProvider >
     
