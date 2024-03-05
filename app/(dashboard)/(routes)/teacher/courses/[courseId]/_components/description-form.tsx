93ad1bdf28c8b8f9347e07ae1e934862b0d26a35
abouba03 <abouba1703@yandex.ru>
2024-03-05 16:38:52 +0300

    j'ai ajoute le parametre "description" dans la page de creation de cours

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -0,0 +1,121 @@
+"use client"
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+
+
+interface DescriptionFormProps {
+    initialData: {
+        description: string;
+    };
+    courseId: string;
+};
+
+const formShema = z.object({
+    description: z.string().min(1, {
+        message: "Description is required",
+    }),
+});
+
+export const DescriptionForm = ({initialData, courseId}: DescriptionFormProps) => {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    const toggleEdit = () => setIsEditing((current) => !current);
+    const router = useRouter();
+
+    const form = useForm<z.infer<typeof formShema>>({
+        resolver: zodResolver(formShema),
+        defaultValues: initialData,
+    });
+
+    const { isSubmitting, isValid } = form.formState;
+
+    const onSubmit = async (values: z.infer<typeof formShema>) => {
+        try{
+            await axios.patch(`/api/courses/${courseId}`, values);
+            toast.success("Course updated");
+            toggleEdit();
+            router.refresh();
+        } catch {
+            toast.error("Something went wrong")
+        }
+    }
+
+    return (
+        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+            <div className="font-medium flex items-center justify-between">
+                Course Description
+                <Button onClick={toggleEdit} variant="ghost">
+                    {isEditing ? (
+                        <> Cancel</>
+                    ) :
+                    (
+                        <>
+                            <Pencil className="h-4 w-4 mr-2" />
+                        Edit description
+                        </>
+                    )}
+                </Button>
+            </div>
+            {!isEditing &&(
+                <p className={cn(
+                    "text-sm mt-2",
+                    !initialData.description && "text-slate-500 italic"
+                )}>
+                    {initialData.description || "No description"}
+                </p>
+            )}
+            {isEditing && (
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mt-4">
+                        <FormField
+                            control={form.control}
+                            name="description"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Textarea
+                                            disabled={isSubmitting}
+                                            placeholder="e.g. 'This courseis about...'"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        >
+                        </FormField> 
+                    
+                        <div className="flex items-cenetr gap-x-2">
+                            <Button
+                                disabled={!isValid || isSubmitting}
+                                type="submit"
+                            >
+                                Save
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+            )}
+        </div>
+    )
+}
