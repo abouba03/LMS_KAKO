ac8d50ed1a9fffce9cd5307fe97e026562f6de41
abouba03 <abouba1703@yandex.ru>
2024-03-15 18:00:12 +0300

    ajout du AttachmentForm (pour les dossier du cours)

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,14 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db"
 import { auth } from "@clerk/nextjs";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
 
 const CourseIdPage = async ({params}: {params: {courseId: string}}) => {
 
@@ -20,7 +21,14 @@ const CourseIdPage = async ({params}: {params: {courseId: string}}) => {
     const course = await db.course.findUnique({
         where: {
             id: params.courseId
-        }
+        },
+        include: {
+            attachments: {
+                orderBy: {
+                    createAt: "desc"
+                },
+            },
+        },
     });
 
     const categories = await db.category.findMany({
@@ -106,6 +114,16 @@ const CourseIdPage = async ({params}: {params: {courseId: string}}) => {
                                 </h2>
                             </div>
                             <PriceForm 
+                                initialData={course}
+                                courseId={course.id}
+                            />
+                        </div>
+                        <div>
+                            <div className="flex items-center gap-x-2">
+                                <IconBadge icon={File} />
+                                <h2 className="text-xl">Ressources & Attachments</h2>
+                            </div>
+                            <AttachmentForm
                                 initialData={course}
                                 courseId={course.id}
                             />
